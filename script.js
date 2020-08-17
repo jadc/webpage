d519a3b762361aa714109e96e63281c88b283cc9
jadc <chehimj@gmail.com>
2020-08-17 13:42:14 -0600

    Revert "what"
    This reverts commit aa250913c19cd148d2a1d425b8536126fb49ecc9, reversing
    changes made to c19b20481ba70c7a8d88a55e780f8430c5e9412e.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let lastID;
 
 (() => {
@@ -52,58 +51,3 @@ let lastID;
   }
 
 })();
-=======
-let lastID;
-
-(() => {
-  // Set age value
-  document.getElementById("age").innerHTML = new Date().getFullYear() - 2003;
-
-  
-  for(let btn of document.getElementsByTagName("button")){
-
-    // Assigns icons to all buttons
-    btn.style.backgroundImage = `url(assets/buttons/${btn.id}.svg)`;
-
-    // Handles button functionality
-    btn.addEventListener("click", () => {
-
-      if(lastID == btn.id) return;
-      lastID = btn.id;
-
-      // Redirecting button
-      if(btn.getAttribute("url") != null){
-        return window.location.href = btn.getAttribute("url");
-      }
-
-      // Displays text button
-      if(btn.getAttribute("text") != null){
-        let text = "An error occured.";
-
-        switch(btn.id){
-          default:
-            break;
-          
-          case "discord":
-            text = "My Discord is <code>jad#0001</code>";
-            break;
-          
-          case "gmail":
-            text = "My Email is <code>mail@jad.red</code><br>Please don't spam it.";
-            break;
-        }
-
-        document.getElementById("info").style.opacity = 0;
-        setTimeout(() => {
-          document.getElementById("info").children[0].innerHTML = text;
-          document.getElementById("info").style.opacity = 1;
-        }, 250);
-        return;
-      }
-
-    });
-
-  }
-
-})();
->>>>>>> d7a1a4d0463e62eeb06cf10da6fe0f0f1d7abaa2
