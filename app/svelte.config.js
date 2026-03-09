6f096a4a28d0e08e1b68fb5ae7c25211c60d8c68
jadc <chehimj@gmail.com>
2026-03-08 21:03:45 -0700

    Replace tabs with spaces

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -5,10 +5,10 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
     kit: {
         adapter: adapter({ fallback: "404.html" }),
-        paths: { base: process.argv.includes("dev") ? "" : process.env.BASE_PATH },
+        paths: { base: process.argv.includes("dev") ? "" : process.env.BASE_PATH }
     },
-	preprocess: [mdsvex()],
-	extensions: [".svelte", ".svx"]
+    preprocess: [mdsvex()],
+    extensions: [".svelte", ".svx"]
 };
 
 export default config;
